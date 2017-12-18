49a4838a0878e74b73772ce669371d9a43e742f0
Tor Selden <tor.selden@autoliv.com>
2017-12-18 20:36:54 +0100

    Day18-2 stub

--- tor_selden-csharp/tor_selden-csharp/Day18.cs
+++ tor_selden-csharp/tor_selden-csharp/Day18.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
 namespace tor_selden_csharp
 {
+    class Prog
+    {
+        public Dictionary<string,long> Registers { get; set; }
+        public Queue<string> MsgQueue { get; set; }
+        public int SentMessages { get; set; }
+        public int RecievedMessages { get; set; }
+        public int CurrentCommand { get; set; }
+    }
+
     class Day18
     {
         static string[] input = File.ReadAllLines(Path.Combine(Program.BasePath, "input18.txt"));
@@ -11,46 +21,62 @@ namespace tor_selden_csharp
         static long lastPlayedSound;
         static long currentCommand;
 
+        public static void B()
+        {
+            var prog0 = new Prog() {Registers = InitializerRegisters(0)};
+            var prog1 = new Prog() {Registers = InitializerRegisters(1)};
+
+            do
+            {
+                
+            } while (prog0.MsgQueue.Count>0 && prog1.MsgQueue.Count > 0);
+            
+        }
+
         internal static void A()
         {
-            InitializerRegisters();
+            registers = InitializerRegisters(0);
 
             bool done = false;
-
             while (!done)
             {
-                //for (long i = 0; i < input.Length; i++)
-                //foreach (var item in input)
-                var command = input[currentCommand].Split(new[] { ' ' });
-
-                switch (command[0])
-                {
-                    case "snd":
-                        Snd(command[1]);
-                        break;
-                    case "set":
-                        Set(command[1], command[2]);
-                        break;
-                    case "add":
-                        Add(command[1], command[2]);
-                        break;
-                    case "mul":
-                        Mul(command[1], command[2]);
-                        break;
-                    case "mod":
-                        Mod(command[1], command[2]);
-                        break;
-                    case "rcv":
-                        done = Rcv(command[1]);
-                        break;
-                    case "jgz":
-                        Jgz(command[1], command[2]);
-                        break;
-                    default:
-                        throw new Exception();
-                }
+                done = ParseCommand();
             }
             Console.WriteLine(lastPlayedSound);
+        }
+
+        public static bool ParseCommand()
+        {
+            var command = input[currentCommand].Split(new[] {' '});
+
+            bool done = false;
+            switch (command[0])
+            {
+                case "snd":
+                    Snd(command[1]);
+                    break;
+                case "set":
+                    Set(command[1], command[2]);
+                    break;
+                case "add":
+                    Add(command[1], command[2]);
+                    break;
+                case "mul":
+                    Mul(command[1], command[2]);
+                    break;
+                case "mod":
+                    Mod(command[1], command[2]);
+                    break;
+                case "rcv":
+                    done = Rcv(command[1]);
+                    break;
+                case "jgz":
+                    Jgz(command[1], command[2]);
+                    break;
+                default:
+                    throw new Exception();
+            }
+            return done;
         }
 
         private static void Jgz(string x, string y)
@@ -137,13 +163,18 @@ namespace tor_selden_csharp
             lastPlayedSound = registers[v];
         }
 
-        private static void InitializerRegisters()
+        public static Dictionary<string, long> InitializerRegisters(int startValue)
         {
+            Dictionary<string, long> registers = new Dictionary<string, long>();
+
             registers.Add("a", 0);
             registers.Add("b", 0);
             registers.Add("f", 0);
             registers.Add("i", 0);
-            registers.Add("p", 0);
+            registers.Add("p", startValue);
+
+            return registers;
         }
+
     }
 }
